aeaeda7217a9784d5a8715f634a5b241c682aea9
Idan Persi <persiidan@gmail.com>
2025-09-21 14:21:48 +0300

    ran terraform format to fix

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,11 +2,11 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name = local.cluster_name
+  name               = local.cluster_name
   kubernetes_version = "1.33"
 
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                 = module.vpc.vpc_id
+  subnet_ids             = module.vpc.private_subnets
   endpoint_public_access = true
 
   eks_managed_node_groups = {
