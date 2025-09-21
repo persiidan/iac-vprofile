43ebd88a61e494c2c4987d0bee8c86a6101ed98b
Idan Persi <persiidan@gmail.com>
2025-09-21 14:12:28 +0300

    updated eks module version and cluster version

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,20 +1,16 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "~> 21.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  name    = local.cluster_name
+  kubernetes_version = "1.33"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
+  endpoint_public_access = true
 
   eks_managed_node_groups = {
+    ami_type = "AL2_x86_64"
     one = {
       name = "node-group-1"
 
